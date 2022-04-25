0756324b5bff78509e51cc882c351126f9e5aa79
samuel.santinelli <samuelsantinelli@gmail.com>
2022-04-25 16:43:56 -0300

    mobileDia25Abril

--- app/src/main/java/com/example/royalapp/Dashboard.java
+++ app/src/main/java/com/example/royalapp/Dashboard.java
@@ -56,24 +56,21 @@ public class Dashboard extends AppCompatActivity {
 
 
 
-
+        //inicia o codigo do piechard
 
         PieChart chart = findViewById(R.id.chart1);
-        chart.setUsePercentValues(true);
+        chart.setUsePercentValues(false);
         chart.getDescription().setEnabled(false);
         chart.setExtraOffsets(5, 10, 5, 5);
 
         chart.setDragDecelerationFrictionCoef(0.95f);
 
 
-        chart.setDrawHoleEnabled(true);
-        chart.setHoleColor(Color.WHITE);
+        chart.setDrawHoleEnabled(false);
 
         chart.setTransparentCircleColor(Color.WHITE);
         chart.setTransparentCircleAlpha(110);
 
-        chart.setHoleRadius(58f);
-        chart.setTransparentCircleRadius(61f);
 
         chart.setDrawCenterText(true);
 
@@ -88,42 +85,32 @@ public class Dashboard extends AppCompatActivity {
         // add a selection listener
 
 
-        chart.animateY(1400, Easing.EaseInOutQuad);
         // chart.spin(2000, 0, 360);
 
         Legend l = chart.getLegend();
         l.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
-        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.RIGHT);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.LEFT);
         l.setOrientation(Legend.LegendOrientation.VERTICAL);
-        l.setDrawInside(false);
-        l.setXEntrySpace(7f);
-        l.setYEntrySpace(0f);
-        l.setYOffset(0f);
+//        l.setDrawInside(false);
+//        l.setXEntrySpace(7f);
+//        l.setYEntrySpace(0f);
+//        l.setYOffset(0f);
 
         // entry label styling
         chart.setEntryLabelColor(Color.WHITE);
         chart.setEntryLabelTextSize(12f);
 
-        String[] parties = new String[] {
-                "Party A", "Party B", "Party C", "Party D", "Party E", "Party F", "Party G", "Party H",
-                "Party I", "Party J", "Party K", "Party L", "Party M", "Party N", "Party O", "Party P",
-                "Party Q", "Party R", "Party S", "Party T", "Party U", "Party V", "Party W", "Party X",
-                "Party Y", "Party Z"
-        };
 
+        //RICHARD - o valor será ejetado nessa arrayList
             ArrayList<PieEntry> entries = new ArrayList<>();
+                entries.add(new PieEntry(1f, "teste"));
+                entries.add(new PieEntry(1f, "teste"));
+                entries.add(new PieEntry(1f, "teste"));
 
-            // NOTE: The order of the entries when being added to the entries array determines their position around the center of
-            // the chart.
-            for (int i = 0; i < 3 ; i++) {
-                entries.add(new PieEntry(1f,
-                        parties[i]));
-            }
 
-            PieDataSet dataSet = new PieDataSet(entries, "Election Results");
+            PieDataSet dataSet = new PieDataSet(entries, "valor pra ser mudado");
 
             dataSet.setDrawIcons(false);
-
             dataSet.setSliceSpace(3f);
             dataSet.setIconsOffset(new MPPointF(0, 40));
             dataSet.setSelectionShift(5f);
@@ -132,22 +119,10 @@ public class Dashboard extends AppCompatActivity {
 
             ArrayList<Integer> colors = new ArrayList<>();
 
-            for (int c : ColorTemplate.VORDIPLOM_COLORS)
+            for (int c : ColorTemplate.PASTEL_COLORS) {
                 colors.add(c);
+            }
 
-            for (int c : ColorTemplate.JOYFUL_COLORS)
-                colors.add(c);
-
-            for (int c : ColorTemplate.COLORFUL_COLORS)
-                colors.add(c);
-
-            for (int c : ColorTemplate.LIBERTY_COLORS)
-                colors.add(c);
-
-            for (int c : ColorTemplate.PASTEL_COLORS)
-                colors.add(c);
-
-            colors.add(ColorTemplate.getHoloBlue());
 
             dataSet.setColors(colors);
             //dataSet.setSelectionShift(0f);
@@ -157,11 +132,6 @@ public class Dashboard extends AppCompatActivity {
             data.setValueTextSize(11f);
             data.setValueTextColor(Color.WHITE);
             chart.setData(data);
-
-            // undo all highlights
-            chart.highlightValues(null);
-
-            chart.invalidate();
 
     }
 
